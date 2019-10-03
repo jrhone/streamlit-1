c648820e90f2e0fe2973982ac5516551fccaf9d5
Jonathan Rhone <rhone.j@gmail.com>
2019-10-03 21:35:18 +0400

    install streamlit

--- setup.sh
+++ setup.sh
@@ -1,12 +1,17 @@
+pip install streamlit==0.47.2
+
 mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"rhone.j@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
+\n\
+[global]\n\
+developmentMode = false\n\
 " > ~/.streamlit/config.toml
